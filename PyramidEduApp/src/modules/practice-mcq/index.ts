759bdc0fcd1663a29c811eeedecc99e4f6b76bb6
Makinthan2001 <mahinthan2001a@gmail.com>
2026-07-18 23:05:14 +0530

    add MCQ history and history detail navigation routes and module exports

--- PyramidEduApp/src/modules/practice-mcq/index.ts
+++ PyramidEduApp/src/modules/practice-mcq/index.ts
@@ -1,1 +1,3 @@
 export { default as MCQPracticeScreen } from "./pages/MCQPracticeScreen";
+export { MCQHistoryScreen } from "./pages/MCQHistoryScreen";
+export { MCQHistoryDetailScreen } from "./pages/MCQHistoryDetailScreen";
